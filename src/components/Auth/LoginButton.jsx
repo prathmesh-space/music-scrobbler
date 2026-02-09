fa6d1ad7a4cfa64f6c27a5591997361b51048f3a
Prathmesh <prathmeshkumbhar33@gmail.com>
2026-02-09 18:56:46 +0530

    Fix auth, routing, and module export issues

--- src/components/Auth/LoginButton.jsx
+++ src/components/Auth/LoginButton.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const LoginButton = ({ onClick }) => {
   return (
-    <div style={{ height: "100vh", display: "grid", placeItems: "center" }}>
-      <button onClick={handleLogin}>Login with Last.fm</button>
-    </div>
+    <div style={{ height: '100vh', display: 'grid', placeItems: 'center' }}>
+      <button onClick={onClick}>Login with Last.fm</button>
+      </div>
   );
 };
 
