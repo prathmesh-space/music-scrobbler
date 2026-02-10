8ee7481c978786962a477d28f2aa78ce5d260711
Prathmesh <prathmeshkumbhar33@gmail.com>
2026-02-11 01:36:21 +0530

    artistImage

--- src/utils/lastfmImage.js
+++ src/utils/lastfmImage.js
@@ -1,4 +1,17 @@
+const LASTFM_PLACEHOLDER_HASH = '2a96cbd8b46e442fc41c2b86b821562f';
+
+const normalizeLastFmUrl = (url = '') => url.replace(/^http:\/\//, 'https://');
+
+const isValidLastFmImageUrl = (url = '') => {
+  if (!url) return false;
+  return !url.includes(LASTFM_PLACEHOLDER_HASH);
+};
+
 export const getLastFmImageUrl = (images = []) => {
-  const firstAvailableImage = images.find((image) => image?.['#text']);
-  return firstAvailableImage?.['#text']?.replace(/^http:\/\//, 'https://') || '';
+  const imageCandidates = [...images]
+    .reverse()
+    .map((image) => normalizeLastFmUrl(image?.['#text'] || ''))
+    .filter((url) => isValidLastFmImageUrl(url));
+
+  return imageCandidates[0] || '';
 };
